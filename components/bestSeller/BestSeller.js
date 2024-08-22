071ab16b39ddd77fee31d9f57bafe7b8e5c49619
Denys <Denys0593@gmail.com>
2024-08-22 15:34:33 +0300

    fix

--- components/bestSeller/BestSeller.js
+++ components/bestSeller/BestSeller.js
@@ -4,7 +4,6 @@ import ProductCard from "../productCard/ProductCard";
 import "./bestSeller.scss";
 
 const BestSeller = ({ data }) => {
-  console.log("xyu");
   return (
     <div className="containerBestSeller">
       <div className="containerBestSeller_wrapper">
