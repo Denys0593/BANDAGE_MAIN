36eb379091ffa3b4ed56ec05e3924c311315a26c
Denys <Denys0593@gmail.com>
2024-09-12 15:33:49 +0300

    apple

--- app/layout.js
+++ app/layout.js
@@ -29,6 +29,18 @@ export default function RootLayout({ children }) {
     <html lang="en" className={monsterrat.className}>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
       </head>
       <body>
         <main>{children}</main>
