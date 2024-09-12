87f63e56cb1ef2089106f788c83f7cc1ec34148d
Denys <Denys0593@gmail.com>
2024-09-12 15:50:32 +0300

    favicon troubles

--- app/layout.js
+++ app/layout.js
@@ -30,6 +30,12 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
           rel="icon"
           href="/icon?<generated>"
           type="image/<generated>"
